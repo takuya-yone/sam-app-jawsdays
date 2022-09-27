5d6abb4853b9acd69461db584c6f4d669f641b5f
Takuya Yonezawa <yonezawa.takuya.yq2@is.naist.jp>
2022-09-28 08:32:52 +0900

    up

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,29 +1,30 @@
 import json
 from aws_lambda_powertools import Logger
-from aws_lambda_powertools.utilities.typing import LambdaContext
-from aws_lambda_powertools.utilities.data_classes import APIGatewayProxyEvent
 from aws_lambda_powertools import Tracer
 from aws_lambda_powertools import Metrics
 from aws_lambda_powertools.metrics import MetricUnit
 
 tracer = Tracer()
 logger = Logger()
-# metrics = Metrics(
-#     namespace="sam-app-jawsdays",
-#     service="HelloWorldFunction")
+metrics = Metrics(
+    namespace="sam-app-jawsdays",
+    service="HelloWorldFunction")
 
-# @metrics.log_metrics
+@metrics.log_metrics
 @tracer.capture_lambda_handler
 @logger.inject_lambda_context(log_event=False)
 def lambda_handler(event, context):
-    # logger.info(event)
-    # print('hello from sam logs!')
-    logger.info('aiueoooo')
-    # metrics.add_metric(
-    #     name="CallCount",
-    #     unit=MetricUnit.Count,
-    #     value=1)
 
+    # Powertools Logger
+    logger.info('aiueo')
+
+    # Add Metic
+    metrics.add_metric(
+        name="CallCount",
+        unit=MetricUnit.Count,
+        value=1)
+
+    # Return Response
     return {
         "statusCode": 200,
         "body": json.dumps({
